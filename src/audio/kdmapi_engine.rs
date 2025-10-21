2e3a3c7258e3ba6bd7247c9ac03e598aab34eb36
Lurmog <72163749+Lurmog@users.noreply.github.com>
2025-10-21 15:43:48 +0200

    Refactor KDMAPI to use kdmapi_rs library

--- src/audio/kdmapi_engine.rs
+++ src/audio/kdmapi_engine.rs
@@ -1,110 +1,57 @@
-#[cfg(windows)]
 pub mod kdmapi {
-    use winapi::um::libloaderapi::{GetModuleHandleA, GetProcAddress};
-    use winapi::shared::minwindef::{BOOL, DWORD};
-    use winapi::um::mmsystem::MMRESULT;
-
+    use kdmapi_rs::KDMAPI as KDMAPILib;
+    use kdmapi_rs::KDMAPIStream;
     use crate::audio::midi_audio_engine::MIDIAudioEngine;
 
-    type InitializeKDMAPIStream = unsafe extern "system" fn() -> BOOL;
-    type TerminateKDMAPIStream = unsafe extern "system" fn() -> BOOL;
-    type SendDirectDataNoBuf = unsafe extern "system" fn(DWORD) -> MMRESULT;
-
     pub struct KDMAPI {
-        supports_kdmapi: bool,
-        kdmapi_initialized: bool,
-
-        init_ptr: Option<InitializeKDMAPIStream>,
-        term_ptr: Option<TerminateKDMAPIStream>,
-        send_ptr: Option<SendDirectDataNoBuf>,
+        stream: Option<KDMAPIStream>,
     }
 
     impl KDMAPI {
         pub fn new() -> Self {
-            
-            let init_ptr = unsafe {
-                let module = GetModuleHandleA("OmniMIDI\0".as_ptr() as *const i8);
-                let func = GetProcAddress(module, "InitializeKDMAPIStream\0".as_ptr() as *const i8);
-                if !(module.is_null() || func.is_null()) {
-                    Some(std::mem::transmute::<_, InitializeKDMAPIStream>(func))
-                } else {
-                    None
+            match KDMAPILib.as_ref() {
+                Ok(_) => {
+                    println!("KDMAPI loaded!");
                 }
-            };
-
-            let term_ptr = unsafe {
-                let module = GetModuleHandleA("OmniMIDI\0".as_ptr() as *const i8);
-                let func = GetProcAddress(module, "TerminateKDMAPIStream\0".as_ptr() as *const i8);
-                if !(module.is_null() || func.is_null()) {
-                    Some(std::mem::transmute::<_, TerminateKDMAPIStream>(func))
-                } else {
-                    None
+                Err(e) => {
+                    println!("[WARNING] KDMAPI needs to be installed in order to use it: {}", e);
                 }
-            };
-
-            let send_ptr = unsafe {
-                let module = GetModuleHandleA("OmniMIDI\0".as_ptr() as *const i8);
-                let func = GetProcAddress(module, "SendDirectDataNoBuf\0".as_ptr() as *const i8);
-                if !(module.is_null() || func.is_null()) {
-                    Some(std::mem::transmute::<_, SendDirectDataNoBuf>(func))
-                } else {
-                    None
-                }
-            };
-
-            let supports_kdmapi = init_ptr.is_some() && term_ptr.is_some() && send_ptr.is_some();
-
-            if supports_kdmapi {
-                println!("KDMAPI loaded!");
-            } else {
-                println!("[WARNING] KDMAPI needs to be installed in order to use it.");
             }
 
             Self {
-                supports_kdmapi,
-                kdmapi_initialized: false,
-
-                init_ptr,
-                term_ptr,
-                send_ptr
+                stream: None,
             }
         }
 
         pub fn init(&mut self) {
-            if self.kdmapi_initialized { return; }
+            if self.stream.is_some() { return; }
 
-            if let Some(initialize_kdmapi_stream) = self.init_ptr.as_ref() {
-                println!("{:?}", initialize_kdmapi_stream);
-                unsafe { initialize_kdmapi_stream(); }
-                self.kdmapi_initialized = true;
-            } else {
-                println!("KDMAPI not found or installed...");
+            match KDMAPILib.as_ref() {
+                Ok(kdmapi) => {
+                    match kdmapi.open_stream() {
+                        Ok(stream) => {
+                            self.stream = Some(stream);
+                        }
+                        Err(e) => {
+                            println!("Failed to start KDMAPI streaming: {}", e);
+                        }
+                    }
+                }
+                Err(e) => {
+                    println!("KDMAPI not found or installed: {}", e);
+                }
             }
         }
 
         pub fn close(&mut self) {
-            if !self.kdmapi_initialized { return; }
-            if !self.supports_kdmapi { return; }
-
-            if let Some(terminate_kdmapi_stream) = self.term_ptr.as_ref() {
-                unsafe { terminate_kdmapi_stream(); }
-            } else {
-                println!("KDMAPI not found or installed...");
-            }
-
-            self.kdmapi_initialized = false;
+            self.stream = None; // Drop the stream
         }
     }
 
-    // i forgot that doing unsafe pointer stuff requires drop, else the app may crash lmfao
     impl Drop for KDMAPI {
         fn drop(&mut self) {
             println!("Closing KDMAPI...");
             self.close();
-
-            self.init_ptr = None;
-            self.send_ptr = None;
-            self.term_ptr = None;
         }
     }
 
@@ -118,50 +65,20 @@ pub mod kdmapi {
         }
 
         fn send_event(&mut self, raw_event: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
-            if !self.supports_kdmapi { return Ok(()); }
-            if !self.kdmapi_initialized { 
+            if self.stream.is_none() {
                 println!("[WARNING] KDMAPI stream was never initialized. Initializing automatically...");
                 self.init_audio();
-                self.kdmapi_initialized = true;
             }
 
-            if let Some(send_direct_data) = self.send_ptr.as_ref() {
-                let ev = (raw_event[0] as u32) | 
+            if let Some(stream) = &self.stream {
+                let ev = (raw_event[0] as u32) |
                     ((raw_event[1] as u32) << 8) |
                     ((raw_event[2] as u32) << 16);
-                unsafe { send_direct_data(ev as DWORD); }
+                stream.send_direct_data(ev);
             } else {
-                println!("KDMAPI not found or installed...");
+                println!("KDMAPI stream not available...");
             }
 
-            Ok(())
-        }
-    }
-}
-
-
-#[cfg(not(windows))]
-pub mod kdmapi {
-    use crate::audio::midi_audio_engine::MIDIAudioEngine;
-
-    pub struct KDMAPI {
-        supports_kdmapi: bool
-    }
-
-    impl KDMAPI {
-        pub fn new() -> Self {
-            println!("KDMAPI is not supported on the current OS.");
-
-            Self {
-                supports_kdmapi: false
-            }
-        }
-    }
-
-    impl MIDIAudioEngine for KDMAPI {
-        fn init_audio(&mut self) { }
-        fn close_stream(&mut self) { }
-        fn send_event(&mut self, raw_event: &[u8]) -> Result<(), Box<dyn std::error::Error>> {
             Ok(())
         }
     }
